2b48dfb722e06e4a12c85a042c4a10214d556bdf
Edson Cabral <edson.cabral@trescon.com.br>
2020-09-22 14:29:45 -0300

    🚧 enable cors in the project

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import setupRoutes from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 setupRoutes(app);
 
 export default app;
