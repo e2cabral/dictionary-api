819630209f6236478230e05ea99bfb862d350384
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-03 16:44:11 -0300

    ♻️ refactor load words by pos controller

--- src/presentation/controllers/word/load-word-by-id.ts
+++ src/presentation/controllers/word/load-word-by-id.ts
@@ -1,16 +1,16 @@
-import word from './../../../main/docs/word-schema';
 import { Word } from '../../../domain/model/word';
 import { Request, Response } from 'express';
 import { ILoadWordById } from '../../../domain/usecases/word/i-load-word-by-id';
+import {WordRepository} from "../../../infra/db/mongodb/word/word-repository";
 
 class LoadWordById implements ILoadWordById {
-    constructor() {}
+    constructor(private readonly wordRepository: WordRepository) {}
 
     async load(req: Request, res: Response): Promise<Response<Word>> {
         const { id } = req.params;
-        const w = await word.findById(id);
+        const w = await this.wordRepository.loadById(`${id}`);
         return res.json(w);
     }
 }
 
-export default new LoadWordById();
+export default new LoadWordById(new WordRepository());
