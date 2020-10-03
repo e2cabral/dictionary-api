51855aa262ea1ca8e0e47dcb93fe5f669ef55cc9
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-03 16:43:45 -0300

    ♻️ refactor load words controller

--- src/presentation/controllers/word/load-words.ts
+++ src/presentation/controllers/word/load-words.ts
@@ -1,18 +1,19 @@
 import { Request, Response } from "express";
-
-import word from "../../../main/docs/word-schema";
-
 import { ILoadWords } from '../../../domain/usecases/word/i-load-words';
 import { Word } from '../../../domain/model/word';
+import {WordRepository} from "../../../infra/db/mongodb/word/word-repository";
 
 class LoadWords implements ILoadWords {
-    constructor() {}
+    constructor(private readonly wordRepository: WordRepository) {}
 
     async load(req: Request, res: Response): Promise<Response<Array<Word>>> {
-        const total = await word.countDocuments();
-        const words = await word.find().skip((Math.random() * total) / 100).limit(100);
-        return res.json(words);
+        try {
+            const words = await this.wordRepository.loadAll();
+            return res.json(words);
+        } catch (e) {
+            return res.send(e.message);
+        }
     }
 }
 
-export default new LoadWords();
+export default new LoadWords(new WordRepository());
