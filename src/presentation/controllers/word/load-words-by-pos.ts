f32635766ff1b0c403e307286920cdbf5b38b893
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-03 16:43:13 -0300

    ♻️ refactor load words by id controller

--- src/presentation/controllers/word/load-words-by-pos.ts
+++ src/presentation/controllers/word/load-words-by-pos.ts
@@ -1,18 +1,18 @@
-import { Word } from './../../../domain/model/word';
-import word from '../../../main/docs/word-schema';
+import { Word } from '../../../domain/model/word';
 import { ILoadWordsByPos } from "../../../domain/usecases/word/i-load-words-by-pos";
 import { Request, Response } from 'express';
+import {WordRepository} from "../../../infra/db/mongodb/word/word-repository";
+import {Pos} from "../../../domain/model/pos";
 
 class LoadWordsByPos implements ILoadWordsByPos {
-    constructor() {}
+    constructor(private readonly wordRepository: WordRepository) {}
 
     async load(req: Request, res: Response): Promise<Response<Array<Word>>> {
         const { pos } = req.query;
-        const words = await word.find({ pos: { $regex: pos, $options: 'i' } }).limit(100);
+        const words = await this.wordRepository.loadByPos(pos as Pos);
 
         return res.json(words);
     }
 }
 
-// export type pos = 'n.' | 'a.' | 'v.' | 'adv.' | 'interj.';
-export default new LoadWordsByPos();
+export default new LoadWordsByPos(new WordRepository());
