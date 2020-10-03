c3a783e1d8603f4e92f4106496f3368042112a4b
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-03 16:41:49 -0300

    ♻️ refactor search words controller

--- src/presentation/controllers/word/search-words.ts
+++ src/presentation/controllers/word/search-words.ts
@@ -1,17 +1,17 @@
 import { Request, Response } from 'express';
-import { Word } from './../../../domain/model/word';
-import word from '../../../main/docs/word-schema';
-import { ISearchWords } from './../../../domain/usecases/word/i-search-words';
+import { Word } from '../../../domain/model/word';
+import { ISearchWords } from '../../../domain/usecases/word/i-search-words';
+import {WordRepository} from "../../../infra/db/mongodb/word/word-repository";
 
 class SearchWords implements ISearchWords {
-    constructor() {}
+    constructor(private readonly wordRepository: WordRepository) {}
 
     async search(req: Request, res: Response): Promise<Response<Array<Word>>> {
-        const { name } = req.query;
-        const words = await word.find({ word: { $regex: name, $options: 'i' } }).limit(100);
+        let { name } = req.query;
+        const words = await this.wordRepository.search(`${name}`);
 
         return res.json(words);
     }
 }
 
-export default new SearchWords();
+export default new SearchWords(new WordRepository());
